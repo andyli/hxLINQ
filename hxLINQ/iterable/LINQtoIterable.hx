25bf8ee93e5afba301fb8d594e1b3e60408a3d20
Andy Li <andy@onthewings.net>
2012-10-06 22:46:18 +0800

    Updated info.

--- hxLINQ/iterable/LINQtoIterable.hx
+++ hxLINQ/iterable/LINQtoIterable.hx
@@ -1,12 +1,3 @@
-/*
-	hxLINQ
-	HaXe version of LINQ. Based on the "LINQ to JavaScript (JSLINQ)":http://jslinq.codeplex.com Project.
-
-	JSLINQ is licensed under the Microsoft Reciprocal License (Ms-RL)
-	Copyright (C) 2009 Chris Pietschmann (http://pietschsoft.com). All rights reserved.
-	The license can be found here: http://jslinq.codeplex.com/license
-*/
-
 package hxLINQ.iterable;
 
 import hxLINQ.*;
